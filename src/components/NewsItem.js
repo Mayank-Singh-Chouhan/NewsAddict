db0914fdb5c796b3922e18d4fd2f2c3a8ae5815f
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2022-07-24 20:50:14 +0530

    Created component structure

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        return (
+            <div>
+                This is our news item2
+            </div>
+        )
+    }
+}
+
+export default NewsItem
