00774320b01ea31d8645f07be066e4bb64c4a767
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2022-07-26 17:52:59 +0530

    Implemented logic to fetch and display data

--- src/components/News.js
+++ src/components/News.js
@@ -2,15 +2,70 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+
+    constructor() {
+        super();
+        console.log("This is a constructor");
+        this.state = {
+            articles: [],
+            loading: false,
+            page: 1
+
+        }
+    }
+
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=f4121c3325d7490284584f2c69780401&page=1&pageSize=20";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
+    }
+
+    handleNextClick = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        }
+        else {
+
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f4121c3325d7490284584f2c69780401&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+        }
+    }
+
+    handlePrevClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f4121c3325d7490284584f2c69780401&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+    }
+
     render() {
         return (
-            <div>
-                This is our news component
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
+            <div className='container my-3'>
+                <h1>NewsAddict - Top HeadLines</h1>
+                <div className="row">
+                    {this.state.articles.map((element) => {
+                        return <div key={element.url} className="col-md-3">
+                            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+                        </div>
+                    })}
+                </div>
+                <hr />
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-primary" onClick={this.handlePrevClick}>&larr; Previous</button>
+
+                    <button type="button" className="btn btn-primary" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
             </div>
         )
     }
